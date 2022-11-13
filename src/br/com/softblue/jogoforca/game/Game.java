920b920b7c9b4319e3fb53be3f4ed488ae04282c
valeskamdev <valeskkitty@gmail.com>
2022-11-12 22:05:46 -0300

    Criando uma classe com apenas um objeto

--- src/br/com/softblue/jogoforca/game/Game.java
+++ src/br/com/softblue/jogoforca/game/Game.java
@@ -1,17 +1,17 @@
 package br.com.softblue.jogoforca.game;
 
+import br.com.softblue.jogoforca.core.Dictionary;
 import br.com.softblue.jogoforca.core.Word;
 
 public class Game {
 
     // método que inicia o jogo
     public void start() {
-        Word word = new Word("casa");
-        word.hasChar('a');
-        word.hasChar('c');
-        word.hasChar('s');
-        System.out.println(word.discovered());
-        System.out.println(word);
-        System.out.println(word.getOriginalWord());
+
+        // criando uma instância da classe Dictionary, existindo apenas uma instancia dessa classe, tornado unica
+        Dictionary d1 = Dictionary.getInstance();
+        System.out.println(d1);
+        Word w1 = d1.nextWord();
+        System.out.println(w1.getOriginalWord());
     }
 }
